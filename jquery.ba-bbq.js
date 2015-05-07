76a9c552679b850aeb68e257ab7be17e547f1b38
eosite <eosite@users.noreply.github.com>
2015-05-07 23:08:02 +0800

    update

--- jquery.ba-bbq.js
+++ jquery.ba-bbq.js
@@ -1285,7 +1285,7 @@
     // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     ( $.browser ? $.browser.msie : ( $.support ? $.support.leadingWhitespace == false : null ) ) 
-    && !supports_onhashchange &&  function(){
+    && !supports_onhashchange && ( function(){
       // Not only do IE6/7 need the "magical" Iframe treatment, but so does IE8
       // when running in "IE7 compatibility" mode.
       
